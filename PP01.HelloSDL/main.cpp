cf4d79279b68a0c9b3b73becbd3b818b1b160dc8
ohgyukwon <20171106@vision.hoseo.edu>
2018-09-11 13:14:42 +0900

    Game Framework

--- PP01.HelloSDL/main.cpp
+++ PP01.HelloSDL/main.cpp
@@ -5,10 +5,10 @@ SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-		g_pWindow = SDL_CreateWindow("PP-1.HelloSDL",
+		g_pWindow = SDL_CreateWindow("Game Framework",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
+			1024, 768, SDL_WINDOW_SHOWN);
 
 		if (g_pWindow != 0) {
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
@@ -18,10 +18,10 @@ int main(int argc, char* args[]) {
 		}
 	}
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
 	SDL_RenderClear(g_pRenderer);
 	SDL_RenderPresent(g_pRenderer);
-	SDL_Delay(5000);
+	SDL_Delay(10000);
 	SDL_Quit();
 	return 0;
 }
