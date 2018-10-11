b2a41b19b9970c9f20a84f2da1b1ca9082b2f8a3
김현도 <20171139@vision.hoseo.edu>
2018-10-11 17:41:51 +0900

    10월 11일 PP08.TextureManager

---
+++ PP08.SingletonTM/Game.cpp
@@ -0,0 +1,53 @@
+#include "Game.h"
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
+		if (m_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		if (!TheTextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer)) {
+			return false;
+		}
+	}
+	else {
+		m_bRunning = false;
+		return false;
+	}
+	return true;
+}
+
+void Game::render() {
+	SDL_RenderClear(m_pRenderer);
+	TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82, m_pRenderer);
+	TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::update() {
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Game::clean() {
+	std::cout << "cleanning game\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::handleEvents() {
+	SDL_Event event;
+	if (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
