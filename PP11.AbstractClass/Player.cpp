4d39d9764062d27c471b9dcb718680a59ebb5db1
김현도 <20171139@vision.hoseo.edu>
2018-10-25 18:04:18 +0900

    10월 25일 PP11.AbstractClass

---
+++ PP11.AbstractClass/Player.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+
+Player::Player(const LoaderParams* pParams) :
+
+SDLGameObject(pParams) {
+
+}
+
+void Player::draw() {
+	SDLGameObject::draw();
+}
+
+void Player::update() {
+	m_x -= 1;
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Player::clean() {
+
+}
