70dee4525ed2dfbe3d0cb21cc5bbedc534ee1b83
Oh Gyu Kwon <kok3954@naver.com>
2018-10-28 17:22:03 +0900

    오타 수정

--- PP12.Vector2D/Vector2D.h
+++ PP12.Vector2D/Vector2D.h
@@ -9,7 +9,7 @@ private:
 public:
 	Vector2D(float x, float y) : m_x(x), m_y(y) {}
 	float getX() { return m_x; }
-	float getX() { return m_y; }
+	float getY() { return m_y; }
 	void setX(float x) { m_x = x; }
 	void setY(float y) { m_y = y; }
 
