da09bc2e1896b78fffe17266f6fbf3eb43fa3b8c
김현도 <20171139@vision.hoseo.edu>
2018-11-01 17:22:46 +0900

    11월 01일 PP13.KInputHandler

---
+++ PP13.KInputHandler/Game.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "TextureManager.h"
+#include "Player.h"
+#include "Enemy.h"
+
+class Game {
+private:
+	Game() {}
+	static Game* s_pInstance;
+	std::map<std::string, SDL_Texture*> m_textureMap;
+	std::vector<GameObject*> m_gameObjects;
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+public:
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void render();
+	bool running() { return m_bRunning; }
+	SDL_Renderer* getRenderer() { return m_pRenderer; }
+	void update();
+	void clean();
+	void quit();
+
+	static Game* Instance() {
+		if (s_pInstance == 0) {
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+};
+
+typedef Game TheGame;
---
+++ PP13.KInputHandler/Player.cpp
@@ -0,0 +1,38 @@
+#include "Player.h"
+
+Player::Player(const LoaderParams* pParams) :
+
+	SDLGameObject(pParams) {
+
+}
+
+void Player::draw() {
+	SDLGameObject::draw();
+}
+
+void Player::update() {
+	m_velocity.setX(0);
+	m_velocity.setY(0);
+	handleInput();
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+	SDLGameObject::update();
+}
+
+void Player::clean() {
+
+}
+
+void Player::handleInput() {
+	if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT)) {
+		m_velocity.setX(2);
+	}
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT)) {
+		m_velocity.setX(-2);
+	}
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP)) {
+		m_velocity.setY(-2);
+	}
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN)) {
+		m_velocity.setY(2);
+	}
+}
