eeebbe55398392c08971ac16a94d22a9cfdb2907
김현도 <20171139@vision.hoseo.edu>
2018-11-01 18:27:36 +0900

    11월 01일 PP14.MInputHandler

---
+++ PP14.MInputHandler/Game.cpp
@@ -0,0 +1,59 @@
+#include "Game.h"
+#include "InputHandler.h"
+
+Game* Game::s_pInstance = 0;
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
+		if (m_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		if (!TheTextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer)) {
+			return false;
+		}
+
+		m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
+		m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
+	}
+	else {
+		m_bRunning = false;
+		return false;
+	}
+	return true;
+}
+
+void Game::render() {
+	SDL_RenderClear(m_pRenderer);
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 150, 255, 255);	// ¹è°æ »ö±ò
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++) {
+		m_gameObjects[i]->draw();
+	}
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::update() {
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++) {
+		m_gameObjects[i]->update();
+	}
+}
+
+void Game::clean() {
+	TheInputHandler::Instance()->clean();
+	std::cout << "cleanning game\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::handleEvents() {
+	TheInputHandler::Instance()->update();
+}
+
+void Game::quit() {
+	m_bRunning = false;
+}
